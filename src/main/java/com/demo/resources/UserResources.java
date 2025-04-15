01ec89b4a3b8306cc876c30f35ea15dbc5697cbe
James Gabriel <jamesgabriel8917@gmail.com>
2025-04-14 21:47:48 -0300

    Mais uma aula concluida

--- src/main/java/com/demo/resources/UserResources.java
+++ src/main/java/com/demo/resources/UserResources.java
@@ -3,6 +3,7 @@ package com.demo.resources;
 import com.demo.model.User;
 import com.demo.repository.UserRepository;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -24,6 +25,12 @@ public class UserResources {
         return user;
     }
 
+    @POST
+    @Path("create")
+    public User createUSer(User user){
+        System.out.println("User created");
+        return user;
+    }
     public List<User> getUsers(){
         return userRepository.getUsers();
     }
